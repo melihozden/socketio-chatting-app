81a787ff23cec2cd7296a518756becf30243b33e
melihozden <melihozden403@gmail.com>
2019-07-29 22:27:33 +0300

    mongodb connection

---
+++ helpers/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_STRING,{ useNewUrlParser: true });
+
+mongoose.connection.on('open',()=>{
+    console.log('MongoDB : Connected.');
+})
+mongoose.connection.on('error',()=>{
+    console.log('MongDB : Error!!');
+})
+
+
+mongoose.Promise = global.Promise ;
+
+
