06aabb46afadb02f6dd621d458ef70aca189c624
melihozden <melihozden403@gmail.com>
2019-07-31 02:22:00 +0300

    string env

--- helpers/db.js
+++ helpers/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_STRING,{ useNewUrlParser: true, useMongoClient:true });
+mongoose.connect(' mongodb://localhost/socketio-chatting-app', { useNewUrlParser: true});
 
 mongoose.connection.on('open',()=>{
     console.log('MongoDB : Connected.');
