59be98f5fe06742754e6b85b85a77757faeb4d77
melihozden <melihozden403@gmail.com>
2019-08-08 01:15:14 +0300

    mongoose useCreateIndex issue

--- helpers/db.js
+++ helpers/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 //     return typeof arg === 'string';
 // }
 
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true});
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useCreateIndex:true});
 
 mongoose.connection.on('open',()=>{
     console.log('MongoDB : Connected.');
