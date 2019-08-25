73ed2ba3cfcd61c7f21003bf5ce755015de0c05b
melihozden <melihozden403@gmail.com>
2019-08-25 14:13:35 +0300

    travis clientID error

--- auth/google.js
+++ auth/google.js
@@ -1,7 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 // models 
 const User = require('../models/users');
@@ -11,32 +10,32 @@ passport.use(
         clientID: process.env.GOOGLE_LOGIN_CLIENT_ID,
         clientSecret: process.env.GOOGLE_LOGIN_SECRET_ID,
         callbackURL: process.env.GOOGLE_LOGIN_CALLBACK_URL
-},
- ((accessToken,refreshToken,profile,done)=>{
-    const data = profile._json;
-    // console.log(profile)
-    //  console.log(data);
-
-    User.findOrCreate({
-        'googleId':data.sub,
     },
-    {
-        name: data.given_name,
-        surname: data.family_name,
-        profilePhotoUrl: data.picture  
-    },(err,user) => {
-        return done(err,user);
-    })
+        ((accessToken, refreshToken, profile, done) => {
+            const data = profile._json;
+            // console.log(profile)
+            //  console.log(data);
 
-})
-));
+            User.findOrCreate({
+                'googleId': data.sub,
+            },
+                {
+                    name: data.given_name,
+                    surname: data.family_name,
+                    profilePhotoUrl: data.picture
+                }, (err, user) => {
+                    return done(err, user);
+                })
 
-passport.serializeUser((user,done) => {
-    done(null,user);
+        })
+    ));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
 });
 
-passport.deserializeUser((user,done) => {
-    done(null,user);
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
 module.exports = passport;
