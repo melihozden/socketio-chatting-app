d20373e86d0fa2e62c2ec593c80042f7883e96d4
melihozden <melihozden403@gmail.com>
2019-09-17 13:40:21 +0300

    socketio-redis adapter for different port numbers

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,4 +1,8 @@
 app.controller('chatController',['$scope',($scope)=>{
     const socket = io.connect("http://localhost:3000");
     console.log('Chat Hi');
+
+    socket.on('hello',() =>{
+        console.log("Hello")
+    })
 }])
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,9 +6,18 @@ const socketApi = {
     io
 }
 
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+     host: process.env.REDIS_URI, 
+     port: process.env.REDIS_PORT
+    }));
+
 io.on('connection',socket =>{
     console.log('Socketio Connection is ON')
+
+    socket.broadcast.emit('hello')
 })
+
 
 module.exports = socketApi;
 
